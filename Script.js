1f17012bd712dc67efd51e08648ec47e8fbb420d
Alexandre VIGIER <alexandre.bouat@yahoo.com>
2016-10-18 11:26:03 +0200

    1ere modif

---
+++ Script.js
@@ -0,0 +1,9 @@
+function SLAM() {
+    document.getElementById("demo").innerHTML = "Le BTS SIO option Solution Logicielles et Applications Métiers (SLAM).</br> L'objectif de ce diplome est de formée l'étudiant à développer , à adapter et à maintenir des solutions applicatives.";
+}
+function SISR() {
+    document.getElementById("demo").innerHTML = "Le BTS SIO option Solutions d'Infrastructure, Systèmes et Réseaux (SISR).</br> L'objet de ce diplome est de formée des techniciens supèrieurs capables de gérer un parc informatique ou d'administrer un réseau en PME.";
+}
+function SIO() {
+	document.getElementById("demo").innerHTML = "Le BTS Services Informatiques aux Organisations (SIO) est le renouveau du BTS informatique de gestion.</br>Son objectif est de formée les étudiant a des postes de techniciens dans le domaine de l'informatique.";
+}
